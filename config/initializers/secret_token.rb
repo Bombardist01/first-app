0cd719d9692ccb8336d3f86a1da22bdb63ce9cc1
Alexand <bombardist01@yandex.ru>
2013-01-25 21:47:23 +0200

    Pervoe sohranenie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f8e5d80131604187a7d3e87acf42fe27ce44337d414111b7c1ac2e54281122486c2adf69c883fd36c094ff75652fe854fab3be90de3114a4a307e01ce29a959c'
